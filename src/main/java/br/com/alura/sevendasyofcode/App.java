801f7bd47f7ee3262a5b68268e66f34bbfc3b986
Yuri Dirickson <yuri.dirickson@usp.br>
2023-02-06 10:54:39 -0300

    Limpando imports

--- src/main/java/br/com/alura/sevendasyofcode/App.java
+++ src/main/java/br/com/alura/sevendasyofcode/App.java
@@ -8,16 +8,11 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.cfg.CoercionAction;
-import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
 
 /**
  * Hello world!
