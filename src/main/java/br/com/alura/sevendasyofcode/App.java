9f15c9e128ce914e816e05c9d3ff1029a48c68a5
Yuri Dirickson <yuri.dirickson@usp.br>
2023-02-06 10:53:40 -0300

    Alterando o jackson para o uso do JsonNode e a numeração dos filmes

--- src/main/java/br/com/alura/sevendasyofcode/App.java
+++ src/main/java/br/com/alura/sevendasyofcode/App.java
@@ -14,7 +14,10 @@ import java.util.stream.IntStream;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.CoercionAction;
+import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
 
 /**
  * Hello world!
@@ -54,7 +57,14 @@ public class App
 
         IntStream.range(0, resultado.size())
             .forEach(idx -> {
-                System.out.println("Filme "+(idx+1)+": "+resultado.get(idx));
+                String numeracao = String.valueOf(idx+1);
+                if(idx+1 < 10){
+                    numeracao = "00"+numeracao;
+                } else if(idx+1 < 100){
+                    numeracao = "0"+numeracao;
+                }
+                
+                System.out.println(String.format("Filme %s: %s", numeracao, resultado.get(idx)));
             });
     }
 
@@ -64,16 +74,22 @@ public class App
     }
 
     public static List<String> parseJsonLib(String json){
-
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> lista = new ArrayList<>();
         try {
             System.out.println("Começando a parsear o resultado usando o Jackson");
-            Map<String, List<Map<String, String>>> parsed = new ObjectMapper()
-                .readValue(json, new TypeReference<Map<String, List<Map<String, String>>>>() {});
-            return parsed.get("items").stream()
-                         .map(item -> item.get("fullTitle"))
-                         .collect(Collectors.toList());
+            JsonNode tree = mapper.readTree(json);
+            
+
+            JsonNode items = tree.get("items");
+            if(items.isArray()){
+                for(JsonNode item : items){
+                    lista.add(item.get("fullTitle").asText());
+                }
+            }
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Erro ao traduzir os dados retornados pelo Jackson", e);
         }
+        return lista;
     }
 }
