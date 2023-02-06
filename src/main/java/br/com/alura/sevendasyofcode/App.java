5af69c1b24a0cf3d31def45ccf72377150ab3dc6
Yuri Dirickson <yuri.dirickson@usp.br>
2023-02-06 09:54:53 -0300

    Estrutura inicial do projeto com a primeira chamada

---
+++ src/main/java/br/com/alura/sevendasyofcode/App.java
@@ -0,0 +1,43 @@
+package br.com.alura.sevendasyofcode;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        String apiKey = args[0];
+        if(apiKey == null || apiKey.isBlank()){
+            throw new IllegalArgumentException("O parâmetro para a APIKey é obrigatório");
+        }
+        URI uri = null;
+        try {
+            System.out.println("Construindo a URI de chamadas da API do IMDB");
+            uri = new URI("https://imdb-api.com/en/API/Top250Movies/"+apiKey);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Erro ao tentar criar a URI de chamada ao IMDB", e);
+        }
+        System.out.println("Criando cliente e chamada para a API");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest
+                    .newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+
+        System.out.println("Executando a chamada");
+        client.sendAsync(request, BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(System.out::println)
+                    .join();
+    }
+}
