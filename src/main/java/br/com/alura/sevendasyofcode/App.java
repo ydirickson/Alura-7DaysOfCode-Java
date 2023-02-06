035bd80806bd94812ef26d766db03ccabd0ad7a9
Yuri Dirickson <yuri.dirickson@usp.br>
2023-02-06 10:37:30 -0300

    Criando a primeira chamada com a bilbioteca Jackson para efetuar o parsing.

--- src/main/java/br/com/alura/sevendasyofcode/App.java
+++ src/main/java/br/com/alura/sevendasyofcode/App.java
@@ -6,6 +6,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Hello world!
@@ -36,7 +45,35 @@ public class App
         System.out.println("Executando a chamada");
         client.sendAsync(request, BodyHandlers.ofString())
                     .thenApply(HttpResponse::body)
-                    .thenAccept(System.out::println)
+                    .thenAccept((App::imprimirResultado))
                     .join();
+    }
+
+    public static void imprimirResultado(String json){
+        List<String> resultado = parseJsonLib(json);
+
+        IntStream.range(0, resultado.size())
+            .forEach(idx -> {
+                System.out.println("Filme "+(idx+1)+": "+resultado.get(idx));
+            });
+    }
+
+    public static List<String> parseJsonManual(String json){
+        List<String> lista = new ArrayList<>();
+        return lista;
+    }
+
+    public static List<String> parseJsonLib(String json){
+
+        try {
+            System.out.println("Começando a parsear o resultado usando o Jackson");
+            Map<String, List<Map<String, String>>> parsed = new ObjectMapper()
+                .readValue(json, new TypeReference<Map<String, List<Map<String, String>>>>() {});
+            return parsed.get("items").stream()
+                         .map(item -> item.get("fullTitle"))
+                         .collect(Collectors.toList());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Erro ao traduzir os dados retornados pelo Jackson", e);
+        }
     }
 }
